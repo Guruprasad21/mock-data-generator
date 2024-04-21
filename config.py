d03605f722b9239a296a065262f224605a79426d
guruprasad <gurup4657@gmail.com>
2024-04-22 00:07:17 +0530

    Code to generate mockdata from Mockaroo

---
+++ config.py
@@ -0,0 +1,13 @@
+# API Credentials
+MOCKAROO_END_POINT = "https://api.mockaroo.com/api/generate.json"
+MOCKAROO_API_KEY = "<YOUR API KEY>"
+
+# MOCKAROO Schemas
+CAMPAIGN_SCHEMA_NAME = "campaigns_schema"
+
+# STAGING INFO
+DIM_CAMPAIGN_STAGE_PATH =  "/home/guru/Codes/edtech_campaign_pipeline/stage_dim_campaign/"
+CAMPAIGN_DETAILS_LANDING_ZONE_PATH = "/home/guru/Codes/edtech_campaign_pipeline/campaign_details_landing_zone/"
+
+
+
